ab8534d4f8d90045110b15c1e6b7dde3288dea12
pop stefan <stefanpop999@gmail.com>
2020-10-04 14:48:26 +0300

    added sentry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Sentry from '@sentry/gatsby';
 import Hero from './Hero/Hero';
 import About from './About/About';
 import Projects from './Projects/Projects';
@@ -6,13 +7,13 @@ import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
 
 const App = () => (
-  <>
+  <Sentry.ErrorBoundary fallback="An error has occured">
     <Hero />
     <About />
     <Projects />
     <Contact />
     <Footer />
-  </>
+  </Sentry.ErrorBoundary>
 );
 
 export default App;
